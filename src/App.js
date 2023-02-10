c5b4a958a75c31b3365b12daeeb305948e00db1d
frederic <flausson@normandiewebschool.fr>
2023-02-10 14:42:42 +0100

    healing + attack

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-
+// import BtnClicker from './components/BtnClicker.js';
 
 function App() {
 
-  const [gameData, setGameData] = useState({
-    worker: 0,
-    gold: 0,
-    goldWorker: 0,
-    goldPerSecond: 0,
-    wood: 0,
-    woodWorker: 0,
-    woodPerSecond: 0,
-    winningCondition: 0,
-    timer: 0,
-  });
-  const [gold, setGold] = useState(50);
-  const [worker, setWorker] = useState(0);
+  // const [gameData, setGameData] = useState({
+  //   worker: 0,
+  //   gold: 0,
+  //   goldWorker: 0,
+  //   goldPerSecond: 0,
+  //   wood: 0,
+  //   woodWorker: 0,
+  //   woodPerSecond: 0,
+  //   winningCondition: 0,
+  //   timer: 0,
+  // });
+  const [gold, setGold] = useState(75);
   const [goldWorker, setGoldWorker] = useState(0);
-  const [goldPerSecond, setGoldPerSecond] = useState(5);
 
   const [wood, setWood] = useState(0);
   const [woodWorker, setWoodWorker] = useState(0);
-  const [woodPerSecond, setWoodPerSecond] = useState(0);
 
+  const [fighter, setFighter] = useState(0);
+  const [healer, setHealer] = useState(0);
+  const [worker, setWorker] = useState(0);
+
+  const[bossHp, setBossHp] = useState(100);
+  const[bossAttack, setBossAttack] = useState(4);
+  // const[bossHeal, setBossHeal] = useState(0);
+
+  const[playerHp, setPlayerHp] = useState(100);
 
 
   /* CLIKS UPDATE */ 
@@ -34,7 +40,6 @@ function App() {
     if(worker >= 1) {
     setGoldWorker(goldWorker + 1)
     setWorker(worker - 1)
-    setGoldPerSecond(goldPerSecond + 0.75)
     }
   }
   function handleClickWood() {
@@ -46,23 +51,73 @@ function App() {
     setGold(gold - 75)
     }
   }
+
+  function handleClickFighter() {
+    if(gold >= 75 && wood >= 75) {
+    setFighter( fighter + 1)
+    setGold(gold - 75)
+    setWood(wood - 75)
+    }
+  }
+
+  function handleClickHealer() {
+    if(gold >= 75 && wood >= 75) {
+    setHealer( healer + 1)
+    setGold(gold - 75)
+    setWood(wood - 75)
+    }
+  }
+
+
   function handleClickWoodWorker() {
     if(worker >= 1) {
     setWoodWorker(woodWorker + 1)
     setWorker(worker - 1)
-    setWoodPerSecond(woodPerSecond + 0.25)
     }
   }
 
 /* RESSOURCES PER SENCODS */
+
+function incom(resource) {
+  if (resource === "gold" ) {
+    let resourceIncom = goldWorker * 5;
+    return resourceIncom
+  }
+  if (resource === "wood" ) {
+    let resourceIncom = woodWorker * 3;
+    return resourceIncom
+  }
+}
+
+function combat(resource) {
+  if (resource === "heal" ) {
+    let healIncom = healer;
+    return healIncom
+  }
+  if (resource === "damage" ) {
+    let damageIncom = fighter;
+    return damageIncom
+  }
+}
+
 useEffect(() => {
   const interval = setInterval(() => {
-    setGold(gold => gold + (goldPerSecond));
-    setWood(wood => wood + (woodPerSecond));
+    setGold(gold => gold + incom("gold"));
+    setWood(wood => wood + incom("wood"));
   }, 1000);
 
   return () => clearInterval(interval);
-}, [goldWorker, goldPerSecond, woodWorker, woodPerSecond]);
+}, [goldWorker, woodWorker]);
+
+
+useEffect(() => {
+  const interval = setInterval(() => {
+    setBossHp(bossHp => bossHp - combat("damage"));
+    setPlayerHp(playerHp => playerHp - bossAttack + combat("heal"));
+  }, 4000);
+
+  return () => clearInterval(interval);
+}, [bossHp, playerHp, bossAttack]);
 
 
 
@@ -70,23 +125,54 @@ useEffect(() => {
 
   return (
     <div className="App">
-      <p>{goldPerSecond} golds/seconds</p>
-      <p>{woodPerSecond} golds/seconds</p>
-      <p>worker</p>
-      <p>{worker}</p>
-      <button onClick={handleClickWorker}>buy worker</button>
+      <div className='header'>
+        <div>
+          <h4>{incom("gold")} gold/seconds</h4>
+          <h4>{incom("wood")} wodd/seconds</h4>
+          <h4>{combat("damage")} damage/seconds</h4>
+          <h4>{combat("heal")} heal/seconds</h4>
+        </div>
 
-      <p>Gold :</p>
-      <p>{gold}</p>
-      <button onClick={handleClickGold}>Mine gold</button>
-      <p>{goldWorker}</p>
-      <button onClick={handleClickGoldWorker}>worker to gold</button>
 
-      <p>wood :</p>
-      <p>{wood}</p>
-      <button onClick={handleClickWood}>Mine wood</button>
-      <p>{woodWorker}</p>
-      <button onClick={handleClickWoodWorker}>worker to wood</button>
+        <div>
+          <h2>worker</h2>
+          <p>{worker}</p>
+          <button onClick={handleClickWorker}>buy worker</button>
+        </div>
+
+        <div>
+          <h2>fighter</h2>
+          <p>{fighter}</p>
+          <button onClick={handleClickFighter}>buy fighter</button>
+        </div>
+
+        <div>
+          <h2>healer</h2>
+          <p>{healer}</p>
+          <button onClick={handleClickHealer}>buy healer</button>
+        </div>
+
+        <div>
+          <h2>Gold :</h2>
+          <p>{gold}</p>
+          <button onClick={handleClickGold}>Mine gold</button>
+          <p>{goldWorker}</p>
+          <button onClick={handleClickGoldWorker}>worker to gold</button>
+        </div>
+
+        <div>
+          <h2>wood :</h2>
+          <p>{wood}</p>
+          <button onClick={handleClickWood}>Mine wood</button>
+          <p>{woodWorker}</p>
+          <button onClick={handleClickWoodWorker}>worker to wood</button>
+        </div>
+      </div>
+
+      <div>
+        <h1>player hp : {playerHp}</h1>
+        <h1>boss hp : {bossHp}</h1>
+      </div>
     </div>
   );
 }
---
+++ src/components/BtnClicker.js
@@ -0,0 +1,17 @@
+import '../App.css';
+import React, { useState, useEffect } from 'react';
+
+
+function BtnClicker(props) {
+
+
+console.log(props.gold)
+
+  return (
+    <div>
+        <p>{props.gold} cazaeazeaze</p>
+    </div>
+  );
+}
+
+export default BtnClicker;
